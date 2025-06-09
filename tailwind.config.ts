639bc4439b3a97975cf2b7fe3acf3bffe0374db0
Jonatha2403 <octavio1995rosado@gmail.com>
2025-06-08 19:23:56 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,21 +28,7 @@ const config: Config = {
         xs: '475px',
       },
       fontFamily: {
-        sans: [
-          'var(--font-sf)',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
+        sans: ['var(--font-sf)', 'sans-serif'], // ✅ Esto vincula tu fuente SF Pro
       },
       colors: {
         primary: {
@@ -101,8 +87,7 @@ const config: Config = {
       },
       boxShadow: {
         sm: '0 1px 2px 0 rgb(0 0 0 / 0.03)',
-        DEFAULT:
-          '0 1px 3px 0 rgb(0 0 0 / 0.07), 0 1px 2px -1px rgb(0 0 0 / 0.07)',
+        DEFAULT: '0 1px 3px 0 rgb(0 0 0 / 0.07), 0 1px 2px -1px rgb(0 0 0 / 0.07)',
         md: '0 4px 6px -1px rgb(0 0 0 / 0.07), 0 2px 4px -2px rgb(0 0 0 / 0.07)',
         lg: '0 10px 15px -3px rgb(0 0 0 / 0.07), 0 4px 6px -4px rgb(0 0 0 / 0.07)',
         xl: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
